2ce76234fecbbdc9b8a30f1a261f8cf7e0f73d5b
CANDELA Nicolas <darkxell.mc@gmail.com>
2022-05-11 17:34:47 +0200

    Compose scene fundamentals

--- Assets/DragDetector.cs
+++ Assets/DragDetector.cs
@@ -2,9 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Singletion like behavior to detect up to 20 drag points on the screen
+/// </summary>
 public class DragDetector : MonoBehaviour
 {
-    // Update is called once per frame
+
+    /// <summary>
+    /// List of callback objects that will be notified on drag events
+    /// </summary>
+    public List<DragCallable> callbacks = new List<DragCallable>(10);
+
+    private Vector2[] positionBuffer = new Vector2[10];
+
     void Update()
     {
         if (Input.touchCount > 0)
@@ -15,5 +25,35 @@ public class DragDetector : MonoBehaviour
             transform.position = touchPosition;
             Debug.Log("Touch input detected at id 0 : " + touchPosition);
         }
+
+        for (int i = 0; i < Input.touchCount; i++)
+        {
+            Vector3 touchPosition = Camera.main.ScreenToWorldPoint(Input.touches[i].position);
+            Vector2 culledPos = new Vector2(touchPosition.x, touchPosition.y);
+            touchPosition.z = 0f;
+            if(i==0)
+                transform.position = touchPosition;
+            foreach(c in callbacks) -> {
+                c.OnDrag(culledPos,culledPos - positionBuffer[i], i);
+            };
+            positionBuffer[i] = culledPos;
+        }
+
     }
+
+
+}
+
+/// <summary>
+/// Interface for world elements that have specific interactions with a drag action.
+/// Will be called by this dragDetector
+/// </summary>
+public interface DragCallable
+{
+
+    /// <summary>
+    /// Event called by a DragDetector each update
+    /// </summary>
+    public abstract void OnDrag(Vector2 Position, Vector2 force, int actionID);
+    
 }
